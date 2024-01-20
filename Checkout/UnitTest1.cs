940f78c9ef8a5f6a546b35629fb71de0efcff6c3
Wuzby <marktihonov7@outlook.com>
2024-01-20 14:39:51 +0000

    2nd unit test several items fail test

--- Checkout/UnitTest1.cs
+++ Checkout/UnitTest1.cs
@@ -3,7 +3,7 @@ namespace Checkout
     public class UnitTest1
     {
         [Fact]
-        public void Scan_AddToTotal_Success()
+        public void Scan_AddToTotalOneItem_Success()
         {
             //Arrange
             Main main = new Main();
@@ -13,6 +13,23 @@ namespace Checkout
 
             //Assert
             Assert.Equal(50, main.GetTotalPrice());
+        }
+
+
+        [Fact]
+        public void Scan_AddToTotalSeveralItemsUnique_Success()
+        {
+            //Arrange
+            Main main = new Main();
+
+            //Act
+            main.Scan("A");
+            main.Scan("B");
+            main.Scan("C");
+            main.Scan("D");
+
+            //Assert
+            Assert.Equal(115, main.GetTotalPrice());
         }
     }
 }
